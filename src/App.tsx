cdb5d5ce7c71218e727da30ca3b999a8e88621b9
Mickael Marques <mickaelpombal@gmail.com>
2019-02-26 21:32:14 +0000

    Added prettier file and formatted all the files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>Standup</p>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>Standup</p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
