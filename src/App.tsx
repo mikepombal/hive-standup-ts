18cf01fe6f4e8d59b783d85a3f9e1dde9b8fb231
Mickael Marques <mickaelpombal@gmail.com>
2019-02-26 22:32:40 +0000

    Used example from AWS to use analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
+import Auth from '@aws-amplify/auth';
+import Analytics from '@aws-amplify/analytics';
 
-class App extends Component {
+import awsconfig from './aws-exports';
+
+// retrieve temporary AWS credentials and sign requests
+Auth.configure(awsconfig);
+// send analytics events to Amazon Pinpoint
+Analytics.configure(awsconfig);
+
+interface Props {}
+interface State {
+    analyticsEventSent: boolean;
+    eventsSent: number;
+    resultHtml: JSX.Element;
+}
+class App extends Component<Props, State> {
+    state = {
+        analyticsEventSent: false,
+        eventsSent: 0,
+        resultHtml: <div />,
+    };
+
+    handleAnalyticsClick = () => {
+        console.log('handleAnalyticsClick');
+        Analytics.record('AWS Amplify Tutorial Event').then(evt => {
+            const url =
+                'https://' +
+                awsconfig.aws_project_region +
+                '.console.aws.amazon.com/pinpoint/home/?region=' +
+                awsconfig.aws_project_region +
+                '#/apps/' +
+                awsconfig.aws_mobile_analytics_app_id +
+                '/analytics/events';
+            let result = (
+                <div>
+                    <p>Event Submitted.</p>
+                    <p>Events sent: {++this.state.eventsSent}</p>
+                    <a href={url} target="_blank">
+                        View Events on the Amazon Pinpoint Console
+                    </a>
+                </div>
+            );
+            this.setState({
+                analyticsEventSent: true,
+                eventsSent: this.state.eventsSent,
+                resultHtml: result,
+            });
+        });
+    };
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <p>Standup</p>
+                    <p>Standup!!!</p>
                 </header>
+                <div className="App-intro">
+                    <button className="App-button" onClick={this.handleAnalyticsClick}>
+                        Generate Analytics Event
+                    </button>
+                    <div>{this.state.eventsSent}</div>
+                    <div>{this.state.resultHtml}</div>
+                </div>
             </div>
         );
     }
