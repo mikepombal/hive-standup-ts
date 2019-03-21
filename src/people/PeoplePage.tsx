36165b781ea12189a75a3a73ad283bf2677cf8d1
Mickael Marques <mickaelpombal@gmail.com>
2019-03-21 18:31:07 +0000

    Added form to allow updating data of one person

--- src/people/PeoplePage.tsx
+++ src/people/PeoplePage.tsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
 import { Connect } from 'aws-amplify-react';
-import { Box, Button, DataTable, Grid, Grommet, Text } from 'grommet';
-import { Checkmark } from 'grommet-icons';
+import { Box, Button, CheckBox, Form, FormField, DataTable, Heading, Layer, Text, TextInput } from 'grommet';
+import { Checkmark, Close, Edit } from 'grommet-icons';
 
 import * as queries from '../graphql/queries';
-import { ListPersonsQuery } from '../API';
-// import { createPerson } from '../graphql/mutations';
+import { ListPersonsQuery, GetPersonQuery } from '../API';
+import { getPerson } from '../graphql/queries';
+import { updatePerson } from '../graphql/mutations';
 
 interface Person {
+    id: string;
     username: string;
     name: string;
     surname: string;
@@ -21,34 +23,65 @@ interface ListPersonsQueryType {
     error: Array<Error>;
 }
 
-const columns = [
-    {
-        property: 'name',
-        header: 'Name',
-    },
-    {
-        property: 'surname',
-        header: 'Surname',
-    },
-    {
-        property: 'username',
-        header: 'GitHub',
-    },
-    {
-        property: 'active',
-        header: 'Is Active',
-        render: (datum: Person) => (datum.active ? <Checkmark /> : null),
-    },
-];
-
 interface Props {}
 
-interface State {}
+interface State {
+    open: boolean;
+    selectedPerson: Person | null;
+}
 
 export default class PeoplePage extends Component<Props, State> {
-    state = {};
+    state: State = {
+        open: false,
+        selectedPerson: null,
+    };
+
+    columns = [
+        {
+            property: 'name',
+            header: 'Name',
+        },
+        {
+            property: 'surname',
+            header: 'Surname',
+        },
+        {
+            property: 'username',
+            header: 'GitHub',
+        },
+        {
+            property: 'active',
+            header: 'Is Active',
+            render: (datum: Person) => (datum.active ? <Checkmark /> : null),
+        },
+        {
+            property: 'id',
+            header: 'Edit',
+            render: (datum: Person) => <Button icon={<Edit />} onClick={() => this.showForm(datum)} />,
+        },
+    ];
+
+    update = async (person: Person) => {
+        console.log('Update me');
+        const result = await API.graphql(graphqlOperation(updatePerson, { input: person }));
+        // @ts-ignore
+        if (!result.error) {
+            this.onClose();
+        }
+    };
+
+    showForm = (person?: Person) => {
+        if (person) {
+            this.setState({ open: true, selectedPerson: person });
+        }
+    };
+
+    onClose = () => {
+        this.setState({ open: false });
+    };
 
     render() {
+        const { open, selectedPerson } = this.state;
         return (
             <Box gridArea="main" justify="center" align="center" background="accent-3">
                 <Connect query={graphqlOperation(queries.listPersons)}>
@@ -63,6 +96,7 @@ export default class PeoplePage extends Component<Props, State> {
                                     ? [
                                           ...acc,
                                           {
+                                              id: item.id,
                                               username: item.username,
                                               name: item.name,
                                               surname: item.surname,
@@ -72,9 +106,36 @@ export default class PeoplePage extends Component<Props, State> {
                                     : acc,
                             []
                         );
-                        return <DataTable columns={columns} data={list} size="medium" />;
+                        return <DataTable columns={this.columns} data={list} size="medium" />;
                     }}
                 </Connect>
+                {open && (
+                    <Layer position="right" full="vertical" modal onClickOutside={this.onClose} onEsc={this.onClose}>
+                        <Box fill="vertical" overflow="auto" width="medium" pad="medium">
+                            <Box flex={false} direction="row" justify="between">
+                                <Heading level="2" margin="none">
+                                    Edit
+                                </Heading>
+                                <Button icon={<Close />} onClick={this.onClose} />
+                            </Box>
+                            <Form
+                                value={selectedPerson || {}}
+                                // @ts-ignore
+                                onSubmit={({ value }) => this.update(value)}
+                            >
+                                <FormField label="GitHub Username" name="username" required />
+                                <FormField label="Name" name="name" required />
+                                <FormField label="Surname" name="surname" required />
+                                // @ts-ignore
+                                <FormField component={CheckBox} label="Active" name="active" />
+                                <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+                                    <Button label="Cancel" onClick={this.onClose} />
+                                    <Button type="submit" label="Update" primary />
+                                </Box>
+                            </Form>
+                        </Box>
+                    </Layer>
+                )}
             </Box>
         );
     }
